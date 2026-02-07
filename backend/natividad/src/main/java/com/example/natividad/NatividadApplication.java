e4cb8f02e9dbde99ac0783af115a606244368d36
LAB13_USER ON WS18 <L13Y17W18@LabHCI.net>
2026-02-07 16:11:39 +0800

    Add initial backend and project structure

---
+++ backend/natividad/src/main/java/com/example/natividad/NatividadApplication.java
@@ -0,0 +1,13 @@
+package com.example.natividad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatividadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatividadApplication.class, args);
+	}
+
+}
---
+++ backend/natividad/src/test/java/com/example/natividad/NatividadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.natividad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatividadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
